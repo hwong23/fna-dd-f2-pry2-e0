294f061a3cb8d189bbabbe90194027e1d411f17e
hwo <hwong23@gmail.com>
2023-07-17 18:50:18 -0500

    upd

--- output.sh
+++ output.sh
@@ -1,6 +1,10 @@
 git checkout output
 git pull
 cp FNA*.docx ~/Downloads/tmpr
-$ open FNA*.docx
+
+f="`ls -t ~/Downloads/tmpr/*.docx | head -1`"
+basedocxFILE=${f##*/};  
+open $basedocxFILE
+
 git checkout main
 
